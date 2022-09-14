7f52fba37474d65bed93b5e6eacb6b385fac4c39
b286951543 <892153580@qq.com>
2022-09-14 11:28:37 +0800

    add

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -2,7 +2,7 @@ package com.example.demo.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix="app") // 设置前缀
+@ConfigurationProperties(prefix="app") // 设置前缀，会自动把 application.yml 里对应的参数注入进来，需要依赖：spring-boot-configuration-processor
 public class AppConfig {
 
     public String getName() {
